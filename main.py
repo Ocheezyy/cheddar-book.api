7d9de6f3bce4b93ef15c5ae4d5df988f5cb1ba83
ocheezyyy <seanodonnellse@gmail.com>
2025-04-10 05:42:40 -0400

    Added cors, currently allowing all origins

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.api import booking, business, staff, service
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="CheddarBook")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(business.router, prefix="/business", tags=["business"])
 app.include_router(staff.router, prefix="/staff", tags=["staff"])
